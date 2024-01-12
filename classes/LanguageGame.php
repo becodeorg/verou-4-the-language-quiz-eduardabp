e78366fb1a27d7262d9cc6a2af738f7e602a1000
Eduarda <eduardabpetry@gmail.com>
2024-01-12 09:34:49 +0100

    Create a loop to iterate through Data class to populate words array

--- classes/LanguageGame.php
+++ classes/LanguageGame.php
@@ -6,12 +6,15 @@ class LanguageGame
 
     public function __construct()
     {
-        // :: is used for static functions
-        // They can be called without an instance of that class being created
-        // and are used mostly for more *static* types of data (a fixed set of translations in this case)
-        foreach (Data::words() as $frenchTranslation => $englishTranslation) {
+        foreach (Data::words() as $dutchTranslation => $englishTranslation) {
             // TODO: create instances of the Word class to be added to the words array
+            $this->words[] = new Word($dutchTranslation, $englishTranslation);
         }
+    }
+
+    // generate random word
+    public function generateRandomWord () {
+
     }
 
     public function run(): void
@@ -25,5 +28,19 @@ class LanguageGame
         // TODO: verify the answer (use the verify function in the word class) - you'll need to get the used word from the array first
         // TODO: generate a message for the user that can be shown
 
+        if (empty($_POST)) {
+            $this->word = $this->generateRandomWord();
+            $_SESSION["Word"] = $this->word;
+
+            // print_r($this->word);
+        } else {
+            if ($_SESSION["Word"]->verify($_POST['player-answer']))
+            {
+                echo 'correct, het antwoord was : ' . $_SESSION["Word"]->getDutchTranslation();
+            } else {
+                echo "fout, het antwoord was : " . $_SESSION["Word"]->getDutchTranslation();
+            }
+            $_SESSION['Word'] = $this->randomWord();
+        }
     }
 }
