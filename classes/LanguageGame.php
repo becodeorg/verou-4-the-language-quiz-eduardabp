7574f57b3c1292b2e203f95e612abb16a6b61278
Eduarda <eduardabpetry@gmail.com>
2024-01-12 14:09:21 +0100

    Fix randomWord function

--- classes/LanguageGame.php
+++ classes/LanguageGame.php
@@ -3,19 +3,21 @@
 class LanguageGame
 {
     private array $words;
-    private string $word;
+    private $word;
 
     public function __construct()
     {
         foreach (Data::words() as $dutchTranslation => $englishTranslation) {
-            // TODO: create instances of the Word class to be added to the words array
             $this->words[] = new Word($dutchTranslation, $englishTranslation);
         }
     }
 
     // generate random word
     public function randomWord () {
-
+        $randomIndex = array_rand($this->words);
+        $this->word = $this->words[$randomIndex];
+        $_SESSION["Word"] = $this->word;
+        return $this->word;
     }
 
     public function run(): void
@@ -26,7 +28,7 @@ class LanguageGame
             $this->word = $this->randomWord();
             $_SESSION["Word"] = $this->word;
         } elseif ($_SERVER["REQUEST_METHOD"] === "POST") {
-            $_SESSION["Word"]->verify($_POST['player-answer']);
+            $_SESSION["Word"]->verify($_POST['player-input']);
             $_SESSION['Word'] = $this->randomWord();
         }
     }
