38f08b86b058a071cbd8e0c18ce8e30f7900ed11
Eduarda <eduardabpetry@gmail.com>
2024-01-12 09:57:49 +0100

    Use run function to check request method

--- classes/LanguageGame.php
+++ classes/LanguageGame.php
@@ -3,6 +3,7 @@
 class LanguageGame
 {
     private array $words;
+    private string $word;
 
     public function __construct()
     {
@@ -13,33 +14,19 @@ class LanguageGame
     }
 
     // generate random word
-    public function generateRandomWord () {
+    public function randomWord () {
 
     }
 
     public function run(): void
     {
-        // TODO: check for option A or B
+        session_start();
 
-        // Option A: user visits site first time (or wants a new word)
-        // TODO: select a random word for the user to translate
-
-        // Option B: user has just submitted an answer
-        // TODO: verify the answer (use the verify function in the word class) - you'll need to get the used word from the array first
-        // TODO: generate a message for the user that can be shown
-
-        if (empty($_POST)) {
-            $this->word = $this->generateRandomWord();
+        if ($_SERVER["REQUEST_METHOD"] === "GET") {
+            $this->word = $this->randomWord();
             $_SESSION["Word"] = $this->word;
-
-            // print_r($this->word);
-        } else {
-            if ($_SESSION["Word"]->verify($_POST['player-answer']))
-            {
-                echo 'correct, het antwoord was : ' . $_SESSION["Word"]->getDutchTranslation();
-            } else {
-                echo "fout, het antwoord was : " . $_SESSION["Word"]->getDutchTranslation();
-            }
+        } elseif ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $_SESSION["Word"]->verify($_POST['player-answer']);
             $_SESSION['Word'] = $this->randomWord();
         }
     }
